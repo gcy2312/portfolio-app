7a8ba095b644da7cfa7c4c79b08a295bb28b5d46
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-30 19:45:12 -0400

    export all components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from "./Navigation/Navigation";
+export { default as Footer } from "./Footer/Footer";
+export { default as Home } from "./Home/Home";
+export { default as About } from "./About/About";
+export { default as Contact } from "./Contact/Contact";
+export { default as Work } from "./Work/Work";
