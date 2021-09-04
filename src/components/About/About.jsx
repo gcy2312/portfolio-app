62f83ce6310fd5cb603cf4924011afc0ccff44a2
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-09-04 10:05:55 -0400

    test animations on scroll

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import { slideInRight } from "react-animations";
+import ScrollAnimation from 'react-animate-on-scroll';
+
+const SlideInRightAnimation = keyframes`${slideInRight}`;
+const SlideInRightDiv = styled.div
+  `animation: 3s ${SlideInRightAnimation};`;
 
 function About() {
   return (
@@ -13,13 +20,17 @@ function About() {
             />
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <SlideInRightDiv>
+              <h1 class="font-weight-light">About</h1>
+            </SlideInRightDiv>
+            <ScrollAnimation animateIn="fadeIn">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+              </p>
+            </ScrollAnimation>
           </div>
         </div>
       </div>
