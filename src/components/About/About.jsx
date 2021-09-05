efe1e71c32c698029c786486e59facbf9c188f1a
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-09-05 17:00:19 -0400

    toolbelt icons + background text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,40 +1,119 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { Row, Col } from 'react-bootstrap';
+
 import styled, { keyframes } from "styled-components";
 import { slideInRight } from "react-animations";
 import ScrollAnimation from 'react-animate-on-scroll';
+
+import html5 from '../../assets/html5.svg';
+import css3 from '../../assets/css3.svg';
+import bootstrap from '../../assets/bootstrap.svg';
+import materialui from '../../assets/materialui.svg';
+import javascript from '../../assets/javascript.svg';
+import nodejs from '../../assets/nodejs.svg';
+import mongodb from '../../assets/mongodb.svg';
+import postgre from '../../assets/postgre.svg';
+import react from '../../assets/react.svg';
+import angular from '../../assets/angular.svg';
+import github from '../../assets/github.svg';
+import postman from '../../assets/postman.svg';
+import npm from '../../assets/npm.svg';
+import redux from '../../assets/redux.svg';
+
+import './about.css';
+import { Typography } from "@material-ui/core";
 
 const SlideInRightAnimation = keyframes`${slideInRight}`;
 const SlideInRightDiv = styled.div
   `animation: 3s ${SlideInRightAnimation};`;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  icons: {
+    padding: theme.spacing(1),
+    width: '90px',
+    height: 'auto',
+    // textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function About() {
+  const classes = useStyles();
+
   return (
     <div id="About" className="about">
       <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <SlideInRightDiv>
-              <h1 class="font-weight-light">About</h1>
-            </SlideInRightDiv>
-            <ScrollAnimation animateIn="fadeIn">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
-              </p>
-            </ScrollAnimation>
-          </div>
-        </div>
+        <Typography id="about-header" variant="h3">About</Typography>
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <Typography id="background-header" variant="h4">Background</Typography>
+            <Typography id="background-text" variant="body1"> I am Genevieve Cyr, a web developer with the tools and experience to help you realize your next project.
+              Whether it be a mobile app or a business website, I can help you materialize your vision.<br />
+              Professional, intuitive and dedicated. I am putting my decade long career an an audio engineer to new use. Using the the skills, both technical and creative, that I have acquired over my careeer, and chanelling them to build and design innovative apps and websites.<br />
+              I am enthusiastic and eager to learn, so don't
+              hesitate if there are other programs you need - I am always happy to broaden my skills!
+            </Typography>
+          </Col>
+          <Col md={6}>
+            {/* <SlideInRightDiv> */}
+            <Typography id="toolbelt-header" variant="h4">Toolbelt</Typography>
+            {/* </SlideInRightDiv> */}
+
+            <div className={classes.root}>
+              <Grid container spacing={1}>
+                <Grid item xs={2}>
+                  <img src={html5} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={css3} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={bootstrap} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={materialui} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={javascript} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={nodejs} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={mongodb} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={postgre} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={react} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={angular} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={github} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={postman} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={npm} width="90px" className={classes.icons}></img>
+                </Grid>
+                <Grid item xs={2}>
+                  <img src={redux} width="90px" className={classes.icons}></img>
+                </Grid>
+              </Grid>
+            </div>
+          </Col>
+        </Row>
       </div>
-    </div>
+    </div >
   );
 }
 
