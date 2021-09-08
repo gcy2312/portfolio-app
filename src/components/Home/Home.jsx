178336dd342596e4d7d6dda61c9e138bf788599c
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-09-07 21:45:46 -0400

    intro code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,66 @@
 import React from "react";
-import { Typography } from '@material-ui/core';
+import { Typography, Container } from '@material-ui/core';
 import { Row, Col } from 'react-bootstrap';
 import Typist from 'react-typist';
 
 import './home.css';
 
+// const genevieveCyr = (name) => {
+//   loadPortfolioApp();
+//   if (name === 'Curious') {
+//     scrollDown();
+//   } else if (name === 'Have a question') {
+//     contactPage();
+//   } else {
+//     codingTime();
+//   }
+// };
+
 function Home() {
   return (
     <div id="Home" className="home">
       <div class="container">
-        <Typography id="home-header" variant="h3">Home</Typography>
+        <Typography id="home-header" variant="h3"></Typography>
 
         <Row className="justify-content-md-center">
 
-          <Col md={5}>
+          <Col md={7}>
+            <Container>
 
-            <Typist className="MyTypist">
-              <span className="my-custom-class"> First Sentenc. Lorem ipsum is the short-hand term for the most popular placeholder text in history, used for decades by graphic designers, web developers, and the publishing industry. e </span>
-              <br />
-              <div className="container">
-                <p> This will be animated after first sentence is complete. Here’s what a paragraph of lorem ipsum looks like: Lorem Ipsum dolor sit amet, </p>
-                <p prop1="val1"> More text.consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              </div>
-              Final sentence
-            </Typist>
+              <Typist avgTypingDelay={20} stdTypingDelay={10} className="typist">
+                <span className="gray">//declare web developer</span><br />
+                <span className="orange">const </span><span className="blue">genevieveCyr </span><span className="pink">= </span>
+                <span className="blue">(</span><span className="yellow">name</span><span className="blue">) </span><span className="orange">{'=> '}</span>
+                <span className="white">{' {'}</span>
+                <br />
+                <div className="onetab">
+                  <span className="blue">loadPortfolioApp</span><span className="white">();</span>
+                  <br />
+                  <span className="pink">if </span><span className="white">(</span><span className="yellow">name </span><span className="pink">=== </span>
+                  <span className="green">'Curious'</span><span className="white">)</span><span className="white">{' {'}</span>
+                  <br />
+                  <div className="twotab">
+                    <span className="blue">scrollDown</span><span className="white">();</span>
+                  </div>
+
+                  <span className="white">{'} '}</span><span className="pink">else if </span><span className="white">(</span><span className="yellow">name </span><span className="pink">=== </span>
+                  <span className="green">'Have a question'</span><span className="white">)</span><span className="white">{' {'}</span>
+                  <br />
+                  <div className="twotab">
+                    <span className="blue">contactPage</span><span className="white">();</span>
+                  </div>
+
+                  <span className="white">{'} '}</span><span className="pink">else</span><span className="white">{' {'}</span>
+                  <br />
+                  <div className="twotab">
+                    <span className="blue">codingTime</span><span className="white">();</span>
+                  </div>
+                  <span className="white">{'}'}</span>
+                </div>
+                <span className="white">{'};'}</span>
+
+              </Typist>
+            </Container>
 
           </Col>
         </Row>
