9f0777e1ae194f755d1a7a3362062546d9cbb93d
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-04 10:52:08 -0400

    update contact form for backend logic

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { TextField, InputAdornment, Button, ButtonGroup, Typography } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+import { init } from 'emailjs-com';
+
+import emailjs from 'emailjs-com';
+
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { AccountCircle, AlternateEmail, Message, Phone, Subject } from '@material-ui/icons';
@@ -8,6 +16,7 @@ import { Row, Col } from 'react-bootstrap';
 import logoImg from '../../assets/logoDark700.png';
 
 import './contact.css'
+init("user_AG0ULrLC9DyJpSyTnnKNn");
 
 
 const useStyles = makeStyles((theme) => ({
@@ -23,6 +32,43 @@ const useStyles = makeStyles((theme) => ({
 
 function Contact() {
   const classes = useStyles();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    const { name, email, telephone, subject, message } = data;
+    try {
+      const templateParams = {
+        name,
+        email,
+        telephone,
+        subject,
+        message
+      };
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      );
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast'
+    });
+  };
+
   return (
     <div id="Contact" className="contact">
       <div class="container">
@@ -32,9 +78,22 @@ function Contact() {
 
           <Col md={8}>
 
-            <form id="form" className={classes.root} noValidate autoComplete="off">
+            <form id="form" className={classes.root} noValidate autoComplete="off"
+              onSubmit={handleSubmit(onSubmit)}
+
+            // action="https://formspree.io/f/mzbywgln"
+            //   method="POST"
+            >
               <TextField id="form-input"
                 required
+                name="name" type="text"
+                {...register('name', {
+                  required: { value: true, message: 'Please enter your name' },
+                  maxLength: {
+                    value: 30,
+                    message: 'Please use 30 characters or less'
+                  }
+                })}
                 label="Name"
                 defaultValue=""
                 variant="outlined"
@@ -45,9 +104,15 @@ function Contact() {
                     </InputAdornment>
                   ),
                 }}
-              />
+              />{errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+
               <TextField id="form-input"
                 required
+                name="email" type="email"
+                {...register('email', {
+                  required: true,
+                  pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                })}
                 label="Email"
                 defaultValue=""
                 variant="outlined"
@@ -58,9 +123,16 @@ function Contact() {
                     </InputAdornment>
                   ),
                 }}
-              />
+              />{errors.email && (
+                <span className='errorMessage'>Please enter a valid email address</span>
+              )}
+
               <TextField id="form-input"
                 label="Phone"
+                name="phone" type="tel"
+                {...register('telephone', {
+                  required: false,
+                })}
                 defaultValue=""
                 variant="outlined"
                 InputProps={{
@@ -73,6 +145,14 @@ function Contact() {
               />
               <TextField id="form-input"
                 label="Subject"
+                name="subject" type="text"
+                {...register('subject', {
+                  required: { value: true, message: 'Please enter a subject' },
+                  maxLength: {
+                    value: 75,
+                    message: 'Subject cannot exceed 75 characters'
+                  }
+                })}
                 defaultValue=""
                 variant="outlined"
                 InputProps={{
@@ -82,9 +162,16 @@ function Contact() {
                     </InputAdornment>
                   ),
                 }}
-              />
+              />{errors.subject && (
+                <span className='errorMessage'>{errors.subject.message}</span>
+              )}
+
               <TextField id="form-message"
                 required
+                name="message" type="text"
+                {...register('message', {
+                  required: true
+                })}
                 label="Message"
                 style={{ margin: 8 }}
                 multiline
@@ -103,7 +190,7 @@ function Contact() {
                 }}
                 defaultValue=""
                 variant="outlined"
-              />
+              /> {errors.message && <span className='errorMessage'>Please enter a message</span>}
 
               {/* <Button size="block" type="submit">Submit form</Button> */}
               <ButtonGroup
@@ -111,14 +198,26 @@ function Contact() {
                 aria-label="outlined primary button group"
                 fullWidth="true"
                 style={{ width: 600 }}>
-                <Button id="formBtn">Send Message</Button>
+                <Button id="formBtn" type="submit">Send Message</Button>
+              </ButtonGroup>
+
+              <ButtonGroup
+                variant="contained"
+                aria-label="outlined primary button group"
+                fullWidth="true"
+                href="files.cv.pdf"
+                download
+                target="_blank"
+                style={{ width: 600 }}>
+                <Button id="formBtn">Download CV</Button>
               </ButtonGroup>
             </form>
+            <ToastContainer />
 
           </Col>
         </Row>
       </div>
-    </div>
+    </div >
   );
 }
 
